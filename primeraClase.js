abd9645cc04b49e83aecd465c78eab8e11835319
frankVillaLobo <frankivil89@gmail.com>
2024-08-05 07:42:18 -0500

    Primer archivo

---
+++ primeraClase.js
@@ -0,0 +1,6 @@
+// tenemos diferentes formas de declarar variables
+
+// var es de las primeras formas que existian pero ya no es muy recomendable usarla
+// debido al scope que tiene la misma que es global
+
+// ahora se usa el metodo 

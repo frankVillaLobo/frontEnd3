369ca6a5761794a331a8b6c53566f5f0e2689c0f
frankVillaLobo <frankivil89@gmail.com>
2024-08-09 10:16:59 -0500

    Cambios de estilos con module.css

--- vite-project/src/Components/Card.jsx
+++ vite-project/src/Components/Card.jsx
@@ -1,13 +1,13 @@
 import Button from "./Button";
-import "../Styles/Card.css";
+import CardStyles from "../Styles/Card.module.css";
 
 const Card = ({recipe}) => {
   // puedo hacer una desestructuracion de los props para poder manejar las variables que trae este objeto de manera individual
 
 // peudo desestructurar desde los mismos parametros de la funcion del componente
   return (
-    <div className="card">
-      <img src={recipe.img} alt="pizzaFoto" />
+    <div className={CardStyles.cardContainer}>
+      <img src={recipe.img} alt="pizzaFoto" className={CardStyles.cardImg} />
       <h3>{recipe.tipo}</h3>
       <p>{recipe.precio}</p>
       <Button>Ver detalle</Button>
--- vite-project/src/Components/Home.jsx
+++ vite-project/src/Components/Home.jsx
@@ -1,9 +1,9 @@
 import {pizzas} from "../Utils/pizzas"
 import Card from "./Card";
-
+import HomeStyle from "../Styles/Home.module.css"
 const Home = () => {
   return (
-    <div>
+    <div className={HomeStyle.homeContainer}>
       <h1>Lista de pizzas</h1>
       <div className="list-container">
         {pizzas.map((pizza) => {
